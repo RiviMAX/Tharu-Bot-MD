40dfe137e1e6b969462849f0e22205e276e2916a
Rivishan Sandeep <87434970+RiviMAX@users.noreply.github.com>
2022-08-07 16:14:04 +0530

    ☺️Tԋαɾυ Bσƚ💞

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
-global.vidmenu = fs.readFileSync("./XeonMedia/theme/Tharu Bot Gif.gif ") //gif and video menu
+global.vidmenu = fs.readFileSync("./XeonMedia/theme/Tharu Bot Gif.gif") //gif and video menu
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
