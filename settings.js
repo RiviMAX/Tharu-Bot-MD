389f6800a6d5c21c0799ce87bac4d350cf74e60b
Rivishan Sandeep <87434970+RiviMAX@users.noreply.github.com>
2022-08-07 16:26:31 +0530

    ☺️Tԋαɾυ Bσƚ💞

--- settings.js
+++ settings.js
@@ -37,8 +37,8 @@ global.APIKeys = {
 
 //aumto functioner
 global.autoTyping = true //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
